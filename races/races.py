913ba13a37ace58fdb9e667f478de8dee3b9bf6e
ab0b0buz <131458484+ab0b0buz@users.noreply.github.com>
2023-04-30 21:14:36 +0300

    Add files via upload

--- races/races.py
+++ races/races.py
@@ -1,34 +1,58 @@
 from pygame import *
-import sys
-sys.setrecursionlimit(10000)
+from random import *
+import math
+
+
 
 mixer.init()
 
 
-
+car100 = sprite.Group()  
 
 clock = time.Clock()
 FPS = 60
 clock.tick(FPS)
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+
 #создаём окно
-
-#sprite1 = sprite.Group()
-
-
-win = display.set_mode((852,480))
+win = display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 display.set_caption('Гонки')
-background = transform.scale(image.load('psdf.jpg'), (852, 480))
+background = transform.scale(image.load('back.png'), (740, 600))
+bg_width = background.get_width()
 
+scroll = 0
+
+tiles = math.ceil(SCREEN_WIDTH / bg_width) + 1
 
 
 class GameSprite(sprite.Sprite):
-    def init__(self, player_image, player_x, player_y, player_speed):
+    def __init__(self, image_mode, size_image, player_x, player_y, player_speed):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.size_image = size_image
+        if image_mode == 1:
+            self.image = transform.scale(image.load('car1.png'), self.size_image)
+
+
+        if image_mode == 2:
+            self.image = transform.scale(image.load('car2.png'), self.size_image)
+
+
+        if image_mode == 3:
+            self.image = transform.scale(image.load('car3.png'), self.size_image)
+
+        if image_mode == 4:
+            self.image = transform.scale(image.load('car4.png'), self.size_image)
+
+
+
         self.speed = player_speed
-        self.pect = self.image.get_rect()
+        self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+
 
     def reset(self):
         win.blit(self.image, (self.rect.x, self.rect.y))
@@ -40,24 +64,75 @@ class Player(GameSprite):
     def update(self):
         keys = key.get_pressed()
         #прописываем движение машины
-        if keys[K_UP] and self.rect.y >= 5:
+        if keys[K_UP] and self.rect.y >= 100:
 
             self.rect.y -= self.speed
 
-        if keys[K_DOWN] and self.rect.y <= 600:
+        if keys[K_DOWN] and self.rect.y <= 430:
 
             self.rect.y += self.speed
 
 
-sprite1 = Player(('pngegg.png'),(150 ,100 ,2))
+class Enemy(GameSprite):
+    
+    def update(self):
+
+        if self.rect.x >= -96:
+
+            self.rect.x -= 4
+        else: 
+            
+            self.rect.x = randint(1000, 1100)
+            
+            self.rect.y = randint(100, 430)
+
+            
+            
+            
+
+            
+
+
+
+
+for i in range(4):
+
+    cars = Enemy(randint(1,3), (95,70), randint(1000,1100),randint(100,300) , 2)
+
+    car100.add(cars)
+
+
+sprite1 = Player(4,(95,70),150 ,270,2)
+
+
+
+
 
 run = True
 while run:
-    win.blit(background, (0, 0))
+
+    #win.blit(background, (0, 0))
+
+    for i in range(0, tiles):
+        win.blit(background,(i * bg_width + scroll,0)) 
+    
+    scroll -= 3
+
+
+    if abs(scroll) > bg_width:
+        scroll = 0
+
+
+
+
     for e in event.get():
         if e.type == QUIT:
             run = False
 
     sprite1.reset()
     sprite1.update()
+
+    car100.draw(win)
+    car100.update()
+
     display.update()
